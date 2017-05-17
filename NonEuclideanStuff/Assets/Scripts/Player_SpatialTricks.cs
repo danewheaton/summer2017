4bf7f5cf2f912c3be0203aa2243bce45e6ff7406
danewheaton <wheaton.dane@gmail.com>
2017-05-17 17:36:36 -0500

    made demo scene illustrating a few problems with current "system" for creating impossible spaces

---
+++ NonEuclideanStuff/Assets/Scripts/Player_SpatialTricks.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_SpatialTricks : MonoBehaviour
+{
+    [SerializeField]
+    Collider doorway01Collider;
+
+    [SerializeField]
+    Transform focalPoint;
+
+    [SerializeField]
+    GameObject[] roomGeo;
+
+    [SerializeField]
+    Material regularGeometryMaterial, hiddenGeometryMaterial;
+
+    private void Update()
+    {
+        print(Vector3.Angle(transform.forward, (transform.position - doorway01Collider.transform.position)));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == doorway01Collider)
+        {
+            if (Vector3.Angle(transform.forward, (transform.position - focalPoint.position)) > Camera.main.fieldOfView)
+            {
+                foreach (GameObject g in roomGeo)
+                {
+                    g.GetComponent<Collider>().enabled = true;
+                    g.GetComponent<Renderer>().material = regularGeometryMaterial;
+                }
+            }
+            else
+            {
+                foreach (GameObject g in roomGeo)
+                {
+                    g.GetComponent<Collider>().enabled = false;
+                    g.GetComponent<Renderer>().material = hiddenGeometryMaterial;
+                }
+            }
+        }
+    }
+}
