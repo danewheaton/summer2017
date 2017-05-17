0d88366fcbc67c44437e4009dac4425c407885ed
danewheaton <wheaton.dane@gmail.com>
2017-05-17 17:41:06 -0500

    commented spatial tricks script

--- NonEuclideanStuff/Assets/Scripts/Player_SpatialTricks.cs
+++ NonEuclideanStuff/Assets/Scripts/Player_SpatialTricks.cs
@@ -15,25 +15,25 @@ public class Player_SpatialTricks : MonoBehaviour
     [SerializeField]
     Material regularGeometryMaterial, hiddenGeometryMaterial;
 
-    private void Update()
-    {
-        print(Vector3.Angle(transform.forward, (transform.position - doorway01Collider.transform.position)));
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other == doorway01Collider)
         {
+            // if you're looking into the middle of the room
             if (Vector3.Angle(transform.forward, (transform.position - focalPoint.position)) > Camera.main.fieldOfView)
             {
+                // activate the collision and visibility of the room
                 foreach (GameObject g in roomGeo)
                 {
                     g.GetComponent<Collider>().enabled = true;
                     g.GetComponent<Renderer>().material = regularGeometryMaterial;
                 }
             }
+
+            // if you're not looking into the middle of the room
             else
             {
+                // deactivate the collision and visibility of the room
                 foreach (GameObject g in roomGeo)
                 {
                     g.GetComponent<Collider>().enabled = false;
