a242b32b45d19b49cd5f17449104b23d9f4bcc32
danewheaton <wheaton.dane@gmail.com>
2017-06-06 00:27:53 -0500

    teleportation scene

---
+++ NonEuclideanStuff/Assets/Scripts/Dane/Player_Teleportation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Teleportation : MonoBehaviour
+{
+    [SerializeField] GameObject targetObject;
+    [SerializeField] float range = 10;
+
+    bool inOriginalRoom;
+
+    private void Update()
+    {
+        bool targetIsCloseAndInView =
+            Vector3.Angle(transform.forward, (transform.position - targetObject.transform.position)) > Camera.main.fieldOfView &&
+            Vector3.Distance(transform.position, targetObject.transform.position) < range;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inOriginalRoom = !inOriginalRoom;
+            transform.position += new Vector3((inOriginalRoom ? 100 : -100), 0, 0);
+
+            if (targetIsCloseAndInView)
+            {
+                targetObject.transform.position += new Vector3((inOriginalRoom ? 100 : -100), 0, 0);
+            }
+        }
+    }
+}
