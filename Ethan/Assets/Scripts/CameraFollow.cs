0736706adb6ce43cfe6496460325c5a91741f9f5
danewheaton <wheaton.dane@gmail.com>
2017-07-29 15:47:15 -0500

    ethan

---
+++ Ethan/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private void Update()
+    {
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        transform.LookAt(player);
+    }
+}
